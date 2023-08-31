05c150076bdfa011811e11edd656417c5a1bcccd
MoeYanHtun <moeyan227970@gmail.com>
2023-09-01 00:08:49 +0630

    usingJs

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const navToggle = document.getElementById('toggle');
 const menuSection = document.getElementById('menu-section');
+const searchbtn = document.getElementById('search__icon');
+const searchBar = document.getElementById('search-bar');
+
+
+
+searchbtn.addEventListener('click', () => {
+    searchBar.classList.toggle('show-search');
+    menuSection.classList.remove('show-menu');
+});
 
 navToggle.addEventListener('click', () => {
-    if (menuSection.style.right === '-200px') {
-        menuSection.style.right = "20px";
-    } else {
-        menuSection.style.right = "-200px";
-    }
-})
-
+    menuSection.classList.toggle('show-menu');
+    searchBar.classList.remove('show-search');
+});
